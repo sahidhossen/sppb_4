57c5987783c9c5758da8bcd7802c93d1f5733497
sifat <sifat@sifat.local>
2020-04-23 17:34:44 +0600

    just changed the indentation

--- builder/src/Canvas.js
+++ builder/src/Canvas.js
@@ -1,10 +1,9 @@
-import React from 'react'; 
-import classnames from 'classnames/bind';
-import GridView from './components/GridView';
-import AddonList from './components/AddonList';
-import {withSelect, withDispatch} from 'store';
-import {compose} from './components/compose';
-
+import React from "react";
+import classnames from "classnames/bind";
+import GridView from "./components/GridView";
+import AddonList from "./components/AddonList";
+import { withSelect, withDispatch } from "store";
+import { compose } from "./components/compose";
 
 class Canvas extends React.Component {
   constructor() {
@@ -19,52 +18,53 @@ class Canvas extends React.Component {
     this.forceUpdate();
   }
 
-    setBlockListRef( node ) {
-        this.wrapperNode = node;
-        // it depents on this.wrapperNode but we can't keep this.wrapperNode in state
-        // Because we need it to be immediately availeble for `focusableTabbable` to work.
-        this.forceUpdate();
-    }
-    
-    render() {
-        let {attributes, mediaQuery, isAddonPicked} = this.props;
-        let {
-            gridGap,
-            gridCol
-        } = attributes; 
+  setBlockListRef(node) {
+    this.wrapperNode = node;
+    // it depents on this.wrapperNode but we can't keep this.wrapperNode in state
+    // Because we need it to be immediately availeble for `focusableTabbable` to work.
+    this.forceUpdate();
+  }
 
-        let style = {
-        "--gw": `${mediaQuery.value}px`,
-        "--gg": gridGap,
-        "--gc": gridCol,
-        "--gr": "auto",
-        "--x": "auto",
-        "--y": "auto",
-        "--w": "auto",
-        "--h": "auto",
-        };
+  render() {
+    let { attributes, mediaQuery, isAddonPicked } = this.props;
+    let { gridGap, gridCol } = attributes;
 
-        const className = classnames({'sppb-builder-wrapper': true,'basegrid': true, 'cursor-draggable': isAddonPicked})
+    let style = {
+      "--gw": `${mediaQuery.value}px`,
+      "--gg": gridGap,
+      "--gc": gridCol,
+      "--gr": "auto",
+      "--x": "auto",
+      "--y": "auto",
+      "--w": "auto",
+      "--h": "auto",
+    };
 
-        return ( 
-            <div style={style} className={className} ref={this.setBlockListRef}> 
-                <GridView container={this.wrapperNode}>
-                    <AddonList/>
-                </GridView>
-            </div>
-        )
-    }
+    const className = classnames({
+      "sppb-builder-wrapper": true,
+      basegrid: true,
+      "cursor-draggable": isAddonPicked,
+    });
+
+    return (
+      <div style={style} className={className} ref={this.setBlockListRef}>
+        <GridView container={this.wrapperNode}>
+          <AddonList />
+        </GridView>
+      </div>
+    );
+  }
 }
 
 export default compose(
-    withSelect( select => {
-        const {getAddon, getActiveMediaQuery, isAddonPicked} = select(); 
-        const {attributes} = getAddon('root');
-        
-        return {
-            mediaQuery: getActiveMediaQuery(),
-            attributes,
-            isAddonPicked: isAddonPicked()
-        }
-    })
+  withSelect((select) => {
+    const { getAddon, getActiveMediaQuery, isAddonPicked } = select();
+    const { attributes } = getAddon("root");
+
+    return {
+      mediaQuery: getActiveMediaQuery(),
+      attributes,
+      isAddonPicked: isAddonPicked(),
+    };
+  })
 )(Canvas);
