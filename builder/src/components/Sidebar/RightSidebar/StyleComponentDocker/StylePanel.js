481ecced9c5d493175fede43dcf38624cab8721d
sifat <sifat@sifat.local>
2020-05-20 17:04:39 +0600

    added panel

--- builder/src/components/Sidebar/RightSidebar/StyleComponentDocker/StylePanel.js
+++ builder/src/components/Sidebar/RightSidebar/StyleComponentDocker/StylePanel.js
@@ -1,42 +1,37 @@
-import React from 'react';
-import {
-    SpacingComponent,
-    Panel
-} from 'style-blocks'; 
+import React from "react";
+import { SpacingComponent, Panel } from "style-blocks";
 
 class StylePanel extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            addonId: null
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      addonId: null,
+    };
+  }
+
+  static getDerivedStateFromProps(prevProps, state) {
+    const { addonId, computeStyle } = prevProps;
+    if (prevProps.addonId !== state.addonId) {
+      computeStyle();
+      return { addonId };
     }
-    
-    static getDerivedStateFromProps( prevProps, state ) {
-        const { addonId, computeStyle } = prevProps;
-        if (prevProps.addonId !== state.addonId) {
-            computeStyle();
-            return { addonId }
-        }
-        return state;
-    }
+    return state;
+  }
 
+  render() {
+    let { styleState, setCssAttributes } = this.props;
+    let { spacing } = styleState;
 
-    render() {
-
-        let {styleState, setCssAttributes} = this.props;
-        let { spacing } = styleState;
-
-        return(
-            <div className="style-panel">
-              <Panel open={true}>
-                <SpacingComponent
-                    style={spacing}
-                    setCssAttributes={setCssAttributes}
-                />
-              </Panel>
-            </div>
-        )
-    }
+    return (
+      <div className="style-panel">
+        <Panel icon="fas fa-arrows-alt-v" title="Spacing">
+          <SpacingComponent
+            style={spacing}
+            setCssAttributes={setCssAttributes}
+          />
+        </Panel>
+      </div>
+    );
+  }
 }
 export default StylePanel;
--- builder/src/style-blocks/components/panel/index.js
+++ builder/src/style-blocks/components/panel/index.js
@@ -1,14 +1,33 @@
-import React from 'react'; 
+import React from "react";
 
 export default class Panel extends React.Component {
-    render() {
-        return (
-            <div className="editor-x-panel">
-                <div className="editor-x-panel-header">Panel Header</div>
-                <div className="panel-x-body">
-                    {this.props.children}
-                </div>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  handleClose(event) {
+    event.preventDefault();
+    this.setState({ ...this.state, isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    const { children, icon, title } = this.props;
+    const { isOpen } = this.state;
+    return (
+      <div className="editor-x-panel">
+        <div className="editor-x-panel-header">
+          <span className={`header-left-icon ${icon}`}></span>
+          <span className="header-title">{title}</span>
+          {!isOpen && <span className="changes">40 top B | 60 InL</span>}
+          <span
+            className={`toggle-icon ${isOpen ? "fas fa-times" : "fas fa-plus"}`}
+            onClick={this.handleClose.bind(this)}
+          ></span>
+        </div>
+        {isOpen && <div className="panel-x-body">{children}</div>}
+      </div>
+    );
+  }
 }
