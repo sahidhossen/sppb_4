9c60213b595bcb427c8690b29531fe8f659a0348
sifat <sifat@sifat.local>
2020-06-08 10:53:17 +0600

    added colorpicker linear and radial functionality

---
+++ builder/src/elements/RangeWithTwoController/index.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+
+export class RangeWithTwoController extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      swap: false,
+    };
+  }
+
+  handleSwap() {
+    this.setState({ swap: !this.state.swap });
+    this.props.onChange({}, "swap");
+  }
+
+  handleChange(event) {
+    const targetName = event.target.name;
+    const targetValue = parseInt(event.target.value);
+    const { leftColorPosition, rightColorPosition } = this.props;
+
+    if (targetName === "left" && targetValue > rightColorPosition.value) {
+      this.props.onChange({ value: targetValue, unit: "%" }, "right");
+    } else if (
+      targetName === "right" &&
+      targetValue < leftColorPosition.value
+    ) {
+      this.props.onChange({ value: targetValue, unit: "%" }, "left");
+    } else {
+      this.props.onChange(
+        {
+          value: targetValue,
+          unit: "%",
+        },
+        targetName
+      );
+    }
+  }
+
+  handleClick(color, event) {
+    event.preventDefault();
+
+    this.props.onClick(event.target.name, color);
+  }
+
+  render() {
+    let gradiant;
+    let {
+      step,
+      gradientType,
+      leftColor,
+      rightColor,
+      angle,
+      extent,
+      position: { x, y },
+      leftColorPosition,
+      rightColorPosition,
+    } = this.props;
+
+    if (gradientType === "radial") {
+      gradiant = `radial-gradient(circle ${extent} at ${x.value}${x.unit} ${y.value}${y.unit}, ${leftColor} ${leftColorPosition.value}%, ${rightColor} ${rightColorPosition.value}%)`;
+    } else {
+      // linear
+      if (angle && angle.value !== 0) {
+        gradiant = `linear-gradient(${angle.value}${angle.unit}, ${leftColor} ${leftColorPosition.value}%, ${rightColor} ${rightColorPosition.value}%)`;
+      } else {
+        gradiant = `linear-gradient(to right, ${leftColor} ${leftColorPosition.value}%, ${rightColor} ${rightColorPosition.value}%)`;
+      }
+    }
+
+    return (
+      <section className="range-slider">
+        <div className="range-slider-control-panel">
+          <input
+            value={leftColorPosition.value}
+            min="0"
+            max="100"
+            step={step}
+            type="range"
+            name="left"
+            onChange={this.handleChange.bind(this)}
+            onClick={this.handleClick.bind(this, leftColor)}
+            style={{ background: gradiant }}
+          />
+          <input
+            value={rightColorPosition.value}
+            min="0"
+            max="100"
+            step={step}
+            type="range"
+            name="right"
+            onChange={this.handleChange.bind(this)}
+            onClick={this.handleClick.bind(this, rightColor)}
+            style={{ background: gradiant }}
+          />
+        </div>
+        <div className="range-slider-bottom">
+          <div
+            className="range-slider-swap fas fa-sync-alt"
+            onClick={this.handleSwap.bind(this)}
+          ></div>
+        </div>
+      </section>
+    );
+  }
+}
+
+RangeWithTwoController.defaultProps = {
+  step: 1,
+  gradientType: "linear",
+  leftColor: "#000000",
+  rightColor: "#ffffff",
+  angle: { value: 0, unit: "deg" },
+};
+
+export default RangeWithTwoController;
