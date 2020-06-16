8567ebcf68fdde6b5bfb7914147ed8aa57a62522
sifat <sifat@sifat.local>
2020-06-16 12:37:58 +0600

    added backgroundItem

--- builder/src/style-blocks/components/backgroundComponent/backgroundItem.js
+++ builder/src/style-blocks/components/backgroundComponent/backgroundItem.js
@@ -1,42 +1,9 @@
 import React, { Component, Fragment } from "react";
-// import { withSelect, withDispatch } from "store";
 import { getBackgroundString } from "../../../lib/utils";
-// import { compose } from "../../../components/compose";
-import PopoverSetting from "../../../components/AddonListAddon/PopoverSetting";
-import SppbPortal from "../../../components/sppbportal/SppbPortal";
-import ColorPickerContainer from "../../../elements/ColorPicker/ColorPickerContainer";
 
 class BackgroundItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isList: false,
-      event: null,
-    };
-  }
-
-  openList(event) {
-    event.persist();
-    this.setState({ isList: true, event });
-    this.props.togglePopoverSettingPanel({
-      status: true,
-      ref: this.button,
-      event,
-    });
-  }
-  reset() {
-    this.setState({ isList: false, event: null });
-    this.props.togglePopoverSettingPanel({
-      status: false,
-      ref: null,
-      contextMenuWrapper: null,
-      event: null,
-      contextStyle: null,
-    });
-  }
-
   render() {
-    const { type, addon, popoverSettingPanel, ...restParams } = this.props;
+    const { type, ...restParams } = this.props;
 
     const background =
       type === "solid"
@@ -53,42 +20,10 @@ class BackgroundItem extends Component {
           <div
             className="editor-x-background-item-right"
             style={background}
-            ref={(ref) => {
-              this.button = ref;
-            }}
-            onClick={this.openList.bind(this)}
           ></div>
         </div>
-        {popoverSettingPanel.status && (
-          <SppbPortal className="popover">
-            <PopoverSetting
-              reset={this.reset.bind(this)}
-              event={this.state.event}
-              target={this.button}
-              addon={addon}
-            >
-              <ColorPickerContainer />
-            </PopoverSetting>
-          </SppbPortal>
-        )}
       </Fragment>
     );
   }
 }
 export default BackgroundItem;
-// export default compose([
-//   withSelect((select) => {
-//     let { popoverSettingPanel } = select();
-//     return {
-//       popoverSettingPanel: popoverSettingPanel(),
-//     };
-//   }),
-//   withDispatch((dispatch) => {
-//     const { togglePopoverSettingPanel } = dispatch();
-//     return {
-//       togglePopoverSettingPanel(status) {
-//         togglePopoverSettingPanel(status);
-//       },
-//     };
-//   }),
-// ])(BackgroundItem);
