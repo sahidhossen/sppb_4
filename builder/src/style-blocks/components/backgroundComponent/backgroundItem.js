92622a4dbda4bd7aa0a32c4ef41152a938c71596
sifat <sifat@sifat.local>
2020-06-16 17:03:22 +0600

    added floating Component and added colorPalette there

--- builder/src/style-blocks/components/backgroundComponent/backgroundItem.js
+++ builder/src/style-blocks/components/backgroundComponent/backgroundItem.js
@@ -1,9 +1,25 @@
 import React, { Component, Fragment } from "react";
-import { getBackgroundString } from "../../../lib/utils";
+import { getBackgroundString, isObject } from "../../../lib/utils";
+import FloatingComponent from "../../../helpers/FloatingComponent";
+import ColorPickerContainer from "../../../elements/ColorPicker/ColorPickerContainer";
 
 class BackgroundItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false,
+      event: null,
+    };
+  }
+
+  toggleColorPicker(event) {
+    event.persist();
+    this.setState({ isOpen: !this.state.isOpen, event });
+  }
+
   render() {
     const { type, ...restParams } = this.props;
+    const { isOpen } = this.state;
 
     const background =
       type === "solid"
@@ -18,9 +34,22 @@ class BackgroundItem extends Component {
             <span className="editor-x-background-title">{type}</span>
           </div>
           <div
+            ref={(ref) => {
+              this.elememnt = ref;
+            }}
             className="editor-x-background-item-right"
             style={background}
+            onClick={this.toggleColorPicker.bind(this)}
           ></div>
+          {isOpen && (
+            <FloatingComponent
+              toggleColorPicker={this.toggleColorPicker.bind(this)}
+              event={this.state.event}
+              target={this.elememnt}
+            >
+              <ColorPickerContainer />
+            </FloatingComponent>
+          )}
         </div>
       </Fragment>
     );
